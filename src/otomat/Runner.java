ac67e3376d4fbbffe357b73ead441381f31c59f4
“User <musagurbuz4661@gmail.com>
2022-08-21 13:03:44 +0200

    distributeur_automatique_de_nourriture

---
+++ src/otomat/Runner.java
@@ -0,0 +1,11 @@
+package otomat;
+
+public class Runner {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("===================================");
+        System.out.println("==== Welcome to Yiyecek Otomat ====");
+        System.out.println("===================================");
+        Urunler.urunlisteleme();
+        Urunler.urunSec();
+    }
+}
